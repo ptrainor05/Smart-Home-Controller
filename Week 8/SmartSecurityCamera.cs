89ae73b7d496f8ea2fa4624644d94ad1a3a20d3b
ptrainor05 <trainor-p5@ulster.ac.uk>
2025-11-10 15:46:55 +0000

    Added superclass / subclass connections

--- Week 8/SmartSecurityCamera.cs
+++ Week 8/SmartSecurityCamera.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Week_8;
 
-namespace Week_8
+namespace SmartHomeController
 {
-    internal class SmartSecurityCamera
+    internal class SmartSecurityCamera : SmartDevice
     {
     }
 }
--- Week 8/SmartThermostat.cs
+++ Week 8/SmartThermostat.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Week_8;
 
-namespace Week_8
+namespace SmartHomeController
 {
-    internal class SmartThermostat
+    internal class SmartThermostat : SmartDevice
     {
     }
 }
